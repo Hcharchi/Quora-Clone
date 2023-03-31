4267698553a7cc56689a0e4661df74cc721dce4b
Hcharchi <117675166+Hcharchi@users.noreply.github.com>
2023-03-31 16:45:44 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const questionAndAnswers = [
+    {
+        id: "1",
+        answeredBy: "badalrana",
+        questionedBy: "harisankar",
+        question: "How does programming work?",
+        answer: "Programming contains a set of instructions for the computer to perform different tasks. Those instructions are executable commands, each having a different purpose.",
+
+    },
+    {
+        id: "2",
+        answeredBy: "soumya ranjan",
+        questionedBy: "Rahul Kumar",
+        question: "What is react?",
+        answer: "React is a User Interface (UI) library React is a tool for building UI components",
+
+    },
+    {
+        id: "3",
+        answeredBy: "siddhu singh",
+        questionedBy: "satish moli",
+        question: "what is blackhole?",
+        answer: "A black hole is a place in space where gravity pulls so much that even light can not get out. The gravity is so strong because matter has been squeezed into a tiny space. This can happen when a star is dying.",
+
+    }
+];
+
+export const questionLists = [
+    {
+        id: '1',
+        questionedBy: 'harisankar',
+        question: "How does programming work??",
+    },
+    {
+        id: "2",
+        questionedBy: "Rahul Kumar",
+        question: "What is react?",
+    },
+    {
+        id: "3",
+        questionedBy: "satish moli",
+        question: "what is blackhole?",
+    },
+    {
+        id: "4",
+        questionedBy: "satish nayak",
+        question: "explain the string theory?",
+
+    },
+    {
+        id: "5",
+        questionedBy: "Rahul Kumar",
+        question: "How Many Dimensions Are There?",
+    }
+
+];
